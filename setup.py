67d5dae319093d7b322e7880d8bcb94064e32851
Pedro Torres <monteirotorres@gmail.com>
2019-09-03 14:23:57 +0100

    Updated Jinja2 requirement

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
   name = 'ProtCHOIR',
   packages = ['ProtCHOIR'],
-  version = '0.1.0',
+  version = '0.1.1',
   description = '[NOT RELEASED] A modeller-based pipeline to generate homo-oligomers.',
   long_description = long_description,
   license='CC-BY-4.0',
